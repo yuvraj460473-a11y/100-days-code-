b0b6b488d411c730146ec67314bc2b236e48337e
yuvraj460473-a11y <yuvraj460473@gmail.com>
2026-09-15 10:33:04 +0530

    Day 01

---
+++ q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    int sum;
+
+    // Taking input from the user
+    scanf("%d %d", &num1, &num2);
+
+    // Calculating the sum
+    sum = num1 + num2;
+
+    // Displaying the result in the exact format required
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
